868fcfd5b3f8b42e5df6be183d0985ebc3846f73
pievos101 <bastianxpfeifer@gmail.com>
2026-05-24 16:07:05 +0200

    fix __init__

--- rankwalk/__init__.py
+++ rankwalk/__init__.py
@@ -1,5 +1,5 @@
 from .data import generate_sbm_graph
-from .gnn import VanillaGNN, train_gnn
+from .gnn import train_gnn
 from .contrastive import contrastive_loss_weighted_fixed, sample_pos_pairs_start_anchor
 from .utils import compute_jaccard_fast
 from .build_temporal_graph import build_temporal_graph
