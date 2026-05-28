cb5e4e0150270f7352296be3ff342a5de060979a
pievos101 <bastianxpfeifer@gmail.com>
2026-05-29 00:49:07 +0200

    final eval script

--- sim_long_TimeAligned_DEEP_FINAL.R
+++ sim_long_TimeAligned_DEEP_FINAL.R
@@ -173,8 +173,10 @@ for (ii in 1:n_iter) {
 
   cat("\n================ ITER", ii, "================\n")
 
-  r_eta = 5
+  r_eta = 3
   r_sigma_diag = rep(5, 5)
+  id = sample(1:5, 1)
+  r_sigma_diag[id] =  sample(5:20, 1)
   print(r_sigma_diag)
 
   Longdat2 <- simLongData(
