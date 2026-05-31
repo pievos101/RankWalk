1ef34342cd37ce23a01c0134a6e23f4e34261db9
pievos101 <bastianxpfeifer@gmail.com>
2026-05-31 22:56:52 +0200

    __ini_

--- rankwalk/__init__.py
+++ rankwalk/__init__.py
@@ -2,4 +2,4 @@ from .data import generate_sbm_graph
 from .gnn import train_gnn
 from .contrastive import contrastive_loss_weighted_fixed, sample_pos_pairs_start_anchor
 from .utils import compute_jaccard_fast
-from .build_temporal_graph import build_temporal_graph_grid, build_temporal_graph, build_temporal_graph_robust_knn, build_temporal_graph_final, build_temporal_graph_aligned
+from .build_temporal_graph import build_temporal_graph_grid, build_temporal_graph_grid2, build_temporal_graph, build_temporal_graph_robust_knn, build_temporal_graph_final, build_temporal_graph_aligned
