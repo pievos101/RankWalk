ac358404aea7d6aaff54f408525700e13413422a
pievos101 <bastianxpfeifer@gmail.com>
2026-05-31 21:51:45 +0200

    graph builder - irregular time

--- rankwalk/build_temporal_graph.py
+++ rankwalk/build_temporal_graph.py
@@ -222,11 +222,18 @@ def build_temporal_graph_grid(
     # =====================================================
     # QUANTILE TIME GRID
     # =====================================================
-    df["visit"] = pd.qcut(
-        df["time"].rank(method="first"),
-        q=n_bins,
+    #df["visit"] = pd.qcut(
+    #    df["time"].rank(method="first"),
+    #    q=n_bins,
+    #    labels=False
+    #).astype(int)
+
+    # UNIFORM
+    df["visit"] = pd.cut(
+        df["time"],
+        bins=n_bins,
         labels=False
-    ).astype(int)
+    )
 
     # =====================================================
     # AGGREGATION (REAL OBS ONLY)
