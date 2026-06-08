4ee5fcd39eefa267c0ec214caab6936af955c3f4
pievos101 <bastianxpfeifer@gmail.com>
2026-06-08 16:49:54 +0200

    fix this for set-up 2

--- sim_long_TimeAligned_DEEP_FINAL.R
+++ sim_long_TimeAligned_DEEP_FINAL.R
@@ -186,16 +186,16 @@ for (ii in 1:n_iter) {
   r_sigma_diag = rep(5, 5)
   id = sample(1:5, 1)
   r_sigma_diag[id] =  sample(5:20, 1)
-  print(r_sigma_diag)
+  #print(r_sigma_diag)
 
-  Longdat2 <- simLongData(
-    ranTimes = FALSE,
-    n_i = 10,
-    eta = r_eta,
-    sigma_diag = r_sigma_diag
-  )
+  #Longdat2 <- simLongData(
+  #  ranTimes = FALSE,
+  #  n_i = 10,
+  #  eta = r_eta,
+  #  sigma_diag = r_sigma_diag
+  #)
 
-  #Longdat2 = simLongData_FPCA_vs_GNN(ranTimes = FALSE)
+  Longdat2 = simLongData_hard(ranTimes = FALSE)
 
 
   Longdat2_wide <- reshape(
